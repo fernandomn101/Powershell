9946dea76ebef6e2b916a5913b6725ecbfb9c0f1
Fernando mohino <Fernandomn101@gmail.com>
2019-02-05 13:57:32 +0100

    VmWare added

---
+++ VmWare/Get Servers info.ps1
@@ -0,0 +1,20 @@
+﻿#Neccesary have installed VmWare modules
+
+$vcenter = "hostname.contoso.corp"
+
+$conection = Connect-VIServer -Server $vcenter -Protocol https
+
+
+#Return  Name,Version,OS,IP-Address,Folder of all the servers in Vcenter
+
+$path = "~\Desktop\servidores.txt"
+$servers = Get-VM
+Remove-Item $path -ErrorAction SilentlyContinue
+foreach($server in $servers){
+	$output = $server.Name +","+$server.Version+","+$server.Guest.OSFullName+","+$server.Guest.IPAddress+","+$server.Folder
+    $output | Out-File $path -Encoding utf8 -Append
+    $output
+}
+
+
+Disconnect-VIServer -Server * -Confirm:$false
