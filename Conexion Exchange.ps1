89ee528f6592fdfa14cf8af0932dc00d61bb4116
Fernando mohino <Fernandomn101@gmail.com>
2019-02-05 12:43:28 +0100

    Subida Conexion Exchange

---
+++ Conexion Exchange.ps1
@@ -0,0 +1,15 @@
+﻿Import-module Msonline -Verbose
+cls
+
+$emailaccount = "john.doe@contoso.com"
+
+$LiveCred =  Get-Credential -UserName $emailaccount -Message "Enter the password"
+$url = "https://ps.outlook.com/PowerShell-LiveID?PSVersion=5.1.16299.637"
+
+try{
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $url -Credential $LiveCred -Authentication Basic -AllowRedirection
+    Import-PSSession $Session 
+    Connect-MsolService –Credential $LiveCred
+}catch{
+    Write-Host ("Error password") -ForegroundColor Yellow
+}
